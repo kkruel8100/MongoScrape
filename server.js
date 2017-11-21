0a71054d08c2ebe93816dcc73d7cbb893543acde
kkruel8100 <kkruel.kruel1@gmail.com>
2017-11-21 07:39:41 -0700

    heroku edits

--- server.js
+++ server.js
@@ -24,19 +24,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
+// requiring routes
+var routes = require("./controllers/controllers.js");
+app.use("/", routes);
+
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect("mongodb://localhost/mongoScraper", {
   useMongoClient: true
 });
-
 // Depending if you are doing a local host or heroku this depends on the switch
 // mongoose.connect("mongodb://heroku_qklsgtbh:1ve7j2b8kaftm428vs89fadnol@ds113746.mlab.com:13746/heroku_qklsgtbh");
-
-// requiring routes
-var routes = require("./controllers/controllers.js");
-app.use("/", routes);
 
 var db = mongoose.connection;
 db.on("error", function(error) {
